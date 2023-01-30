deedf316856cb1882b1e1a220bab8634ba97c296
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2023-01-31 00:04:30 +0900

    vectorize

--- opticalsimulation/opticalsimulation.py
+++ opticalsimulation/opticalsimulation.py
@@ -27,11 +27,31 @@ def reflectance(param, eta0):
         return (np.abs((eta0*param[0]-param[1])/(eta0*param[0]+param[1]))**2)[0]
     return tuple(map(_reflectance,param,eta0))
 
-def calc_matrix(layers,n0,angle,lam):
-    mat = (np.eye(2),np.eye(2))
-    for layer in layers:
-        mat = tuple(map(lambda m1,m2:np.dot(m1,m2),mat,characteristic_matrix(layer[lam],layer['d'],n0[lam],angle,lam)))
-    return mat
+# def calc_matrix(layers,n0,angle,lam):
+#     mat = (np.eye(2),np.eye(2))
+#     for layer in layers:
+#         mat = tuple(map(lambda m1,m2:np.dot(m1,m2),mat,characteristic_matrix(layer[lam],layer['d'],n0[lam],angle,lam)))
+#     return mat
+
+def calc_matrix(ns,ds,n0,n1,theta,lam):
+    length = len(lam)
+    eta = admittance(n1,n0,theta)
+    
+    v_s = tuple([np.array([np.ones(length),eta[0]]),
+                 np.array([np.ones(length),eta[1]])])
+    
+    mat = tuple([np.array([[np.ones(length),np.zeros(length)],[np.zeros(length),np.ones(length)]]),
+                 np.array([[np.ones(length),np.zeros(length)],[np.zeros(length),np.ones(length)]])])
+    for n,d in zip(ns,ds):
+        m = characteristic_matrix(n, d, n0,theta,lam)
+        mat = tuple([np.array([[mat[0][0,0]*m[0][0,0]+mat[0][0,1]*m[0][1,0], mat[0][0,0]*m[0][0,1]+mat[0][0,1]*m[0][1,1]],
+                               [mat[0][1,0]*m[0][0,0]+mat[0][1,1]*m[0][1,0], mat[0][1,0]*m[0][0,1]+mat[0][1,1]*m[0][1,1]]]),
+                     np.array([[mat[1][0,0]*m[1][0,0]+mat[1][0,1]*m[1][1,0], mat[1][0,0]*m[1][0,1]+mat[1][0,1]*m[1][1,1]],
+                               [mat[1][1,0]*m[1][0,0]+mat[1][1,1]*m[1][1,0], mat[1][1,0]*m[1][0,1]+mat[1][1,1]*m[1][1,1]]])])
+
+    param = tuple([np.array([[mat[0][0,0]*v_s[0][0]+mat[0][0,1]*v_s[0][1]],[mat[0][1,0]*v_s[0][0]+mat[0][1,1]*v_s[0][1]]]),
+                   np.array([[mat[1][0,0]*v_s[1][0]+mat[1][0,1]*v_s[1][1]],[mat[1][1,0]*v_s[1][0]+mat[1][1,1]*v_s[1][1]]])])
+    return param
 
 def transmittance(param,eta0,eta_s):
     def _transmittance(param,eta0,eta_s):
--- test/test_opticalsimulation.py
+++ test/test_opticalsimulation.py
@@ -48,38 +48,31 @@ def test_reflectance(n,d,n0,n_sub,angle,lam,ref):
     eta0= admittance(n0,n0,angle)
     assert np.all(np.isclose(reflectance(param,eta0),ref))
 
-@pytest.mark.parametrize(('layers','n0','n_sub','angle','lam','ref'),[
-    ([],{550:1.0},{550:1.5},0.0,550,(0.04,0.04)),
-    ([{'d':0, 550:1.0}],{550:1.0},{550:1.5},0.0,550,(0.04,0.04)),
-    ([{'d':0, 550:1.5}],{550:1.0},{550:1.5},0.0,550,(0.04,0.04)),
-    ([{'d':100, 550:1.0}],{550:1.0},{550:1.5},0.0,550,(0.04,0.04)),
-    ([{'d':100, 550:1.5}],{550:1.0},{550:1.5},0.0,550,(0.04,0.04)),
-    ([{'d':100, 550:1.0},{'d':100, 550:1.5}],{550:1.0},{550:1.5},0.0,550,(0.04,0.04)),
+@pytest.mark.parametrize(('ns','ds','n0','n1','theta','lam','ref'),[
+    ([],[],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.04,0.04)),
+    ([1.0],[0],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.04,0.04)),
+    ([1.5],[0],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.04,0.04)),
+    ([1.0],[100],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.04,0.04)),
+    ([1.5],[100],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.04,0.04)),
+    ([1.0,1.5],[100,100],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.04,0.04)),
 ])
-def test_calc_matrix(layers,n0,n_sub,angle,lam,ref):
-    mat = calc_matrix(layers,n0,angle,lam)
-    eta_sub = admittance(n_sub[lam],n0[lam],angle)
-    
-    vec = (np.array([[1],[eta_sub[0]]]),np.array([[1],[eta_sub[1]]]))
-    param = tuple(map(lambda m,v:np.dot(m,v),mat,vec))
+def test_reflectance(ns,ds,n0,n1,theta,lam,ref):
+    eta0 = admittance(n0,n0,theta)
+    param = calc_matrix(ns,ds,n0,n1,theta,lam)
 
-    eta0= admittance(n0[lam],n0[lam],angle)
     assert np.all(np.isclose(reflectance(param,eta0),ref))
 
-@pytest.mark.parametrize(('layers','n0','n_sub','angle','lam','trans'),[
-    ([],{550:1.0},{550:1.5},0.0,550,(0.96,0.96)),
-    ([{'d':0, 550:1.0}],{550:1.0},{550:1.5},0.0,550,(0.96,0.96)),
-    ([{'d':0, 550:1.5}],{550:1.0},{550:1.5},0.0,550,(0.96,0.96)),
-    ([{'d':100, 550:1.0}],{550:1.0},{550:1.5},0.0,550,(0.96,0.96)),
-    ([{'d':100, 550:1.5}],{550:1.0},{550:1.5},0.0,550,(0.96,0.96)),
-    ([{'d':100, 550:1.0},{'d':100, 550:1.5}],{550:1.0},{550:1.5},0.0,550,(0.96,0.96)),
+@pytest.mark.parametrize(('ns','ds','n0','n1','theta','lam','trans'),[
+    ([],[],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.96,0.96)),
+    ([1.0],[0],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.96,0.96)),
+    ([1.5],[0],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.96,0.96)),
+    ([1.0],[100],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.96,0.96)),
+    ([1.5],[100],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.96,0.96)),
+    ([1.0,1.5],[100,100],np.array([1.0]),np.array([1.5]),0.0,np.array([550]),(0.96,0.96)),
 ])
-def test_transmittance(layers,n0,n_sub,angle,lam,trans):
-    mat = calc_matrix(layers,n0,angle,lam)
-    eta_sub = admittance(n_sub[lam],n0[lam],angle)
-    
-    vec = (np.array([[1],[eta_sub[0]]]),np.array([[1],[eta_sub[1]]]))
-    param = tuple(map(lambda m,v:np.dot(m,v),mat,vec))
+def test_transmittance(ns,ds,n0,n1,theta,lam,trans):
 
-    eta0= admittance(n0[lam],n0[lam],angle)
-    assert np.all(np.isclose(transmittance(param,eta0,eta_sub),trans))
+    eta0 = admittance(n0,n0,theta)
+    eta1 = admittance(n1,n0,theta)
+    param = calc_matrix(ns,ds,n0,n1,theta,lam)
+    assert np.all(np.isclose(transmittance(param,eta0,eta1),trans))
