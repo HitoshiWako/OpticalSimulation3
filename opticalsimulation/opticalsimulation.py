378d7e9c11c821d9c7df210a63f65c8c1fa7dbd0
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2023-02-19 18:46:11 +0900

    calc_spectra

--- opticalsimulation/opticalsimulation.py
+++ opticalsimulation/opticalsimulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import copy
 
 Y0 = np.sqrt(8.8541878128/1.25663706212)*1e-3
 
@@ -57,3 +58,48 @@ def transmittance(param,eta0,eta_s):
     def _transmittance(param,eta0,eta_s):
         return (4*eta0*eta_s.real/np.abs(eta0*param[0]+param[1])**2)[0]
     return tuple(map(_transmittance, param, eta0,eta_s))
+
+def absorb(n1,thickness,n0,theta,lam):
+    phi = snell(n1,n0,theta)
+    return np.exp(-8*np.pi*np.abs(n1.imag)*thickness*1000/(lam*np.cos(phi)))
+
+def calc_spectra(n0,n1,n2,theta,front_ns,front_ds,back_ns,back_ds,t_sub, lam):
+     f_ns = copy(front_ns)
+     f_ds = copy(front_ds)
+     b_ns = copy(back_ns)
+     b_ds = copy(back_ds)
+     
+     phi1 = snell(n1,n0,theta)
+     # phi2 = snell(n2,n0,theta)
+    
+     eta0 = admittance(n0,n0,theta)
+     eta1 = admittance(n1,n0,theta)
+     eta2 = admittance(n2,n0,theta)
+     
+     front_forward = calc_matrix(f_ns,f_ds,n0,n1,theta,lam)
+     f_ns.reverse()
+     f_ds.reverse()
+     front_backward = calc_matrix(f_ns,f_ds,n1,n0,phi1,lam)
+     
+     # back_forward = calc_matrix(b_ns,b_ds,n2,n1,phi2,lam)
+     b_ns.reverse()
+     b_ds.reverse()
+     back_backward = calc_matrix(b_ns,b_ds,n1,n2,phi1,lam)
+     
+     r01 = reflectance(front_forward,eta0)
+     r10 = reflectance(front_backward,eta1)
+     t01 = transmittance(front_forward,eta0,eta1)
+     t10 = transmittance(front_backward,eta1,eta0)
+     
+     r12 = reflectance(back_backward,eta1)
+     t12 = transmittance(back_backward,eta1,eta2) 
+
+
+     beta = absorb(n1,t_sub,n0,theta,lam)
+
+     r = ((r01[0]+ t01[0]*r12[0]*t10[0]*beta)/(1-r10[0]*r12[0]*beta), 
+          (r01[1]+ t01[1]*r12[1]*t10[1]*beta)/(1-r10[1]*r12[1]*beta))
+     t = (t01[0]*t12[0]*np.sqrt(beta)/(1-r10[0]*r12[0]*beta),
+          t01[1]*t12[1]*np.sqrt(beta)/(1-r10[1]*r12[1]*beta))
+
+     return (r01,t01,r,t)
