1215eb2a47c0f155c4f00a47aa5244ce99e872fd
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2023-02-23 23:02:41 +0900

    setup table

---
+++ models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, Float, String
+
+from settings import Base
+
+class Material(Base):
+    __tablename__ = 'material'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        return "<Material(name={})>".format(
+            self.name,
+        )
+
+class OpticalIndex(Base):
+    __tablename__ = 'opticalindex'
+
+    id = Column(Integer, primary_key=True)
+    material_id = Column(Integer)
+    wavelength = Column(Float)
+    n = Column(Float)
+    k = Column(Float)
+
+    def __repr__(self):
+        return "<Optical Index(material id={}, wavelength={}, n={}, k={})>".format(
+            self.material_id,
+            self.wavelength,
+            self.n,
+            self.k
+        )
