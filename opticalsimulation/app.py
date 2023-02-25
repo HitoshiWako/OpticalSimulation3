da52351a2c324d00fa1d044af97096de802fd8be
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2023-02-25 20:05:21 +0900

    line plot

--- opticalsimulation/app.py
+++ opticalsimulation/app.py
@@ -1,35 +1,19 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, html, dcc
-import plotly.express as px
-import pandas as pd
+from dash import Dash, html,dcc
+import numpy as np
+import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 
+x = np.arange(10)
+fig = go.Figure(
+    data=go.Scatter(x=x,y=x**2,mode='lines')
+)
+
 app = Dash(external_stylesheets=[dbc.themes.SPACELAB])
-#app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+app.layout = dbc.Container([
+    html.H1("Optical Index"),
+    html.Hr(),
+    dbc.Col(dcc.Graph(figure=fig)),
 ])
 
 if __name__ == '__main__':
