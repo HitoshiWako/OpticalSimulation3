f80e7f46357de9eaf38b694b678a337a330f84ea
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2023-03-04 15:01:49 +0900

    load from URL

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html,dcc, dash_table,Input,Output,State
+from dash import Dash, html,dcc, dash_table,Input,Output,State,ctx
 import numpy as np
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
@@ -13,12 +13,28 @@ app.layout = dbc.Container([
     html.H1("Optical Index"),
     html.Hr(),
     dbc.Col(dcc.Graph(id='nk_graph')),
+    dbc.Row([
+        dbc.Col([dbc.Input(id='input_url',placeholder='Input URL',type='url')]),
+        dbc.Col([dbc.Button('Load',id='input_button',color='primary',n_clicks=0)]),
+    ]),
+    html.Br(),
     dash_table.DataTable(id='material_list_table',
-                         data=table_data,
+                         data = [],
                          columns=[{"id":"name","name":"Material"}],
                          style_cell={'textAlign':'left'},
                          row_selectable='single')
 ])
+
+@app.callback(
+    Output('material_list_table','data'),
+    Output('input_url','value'),
+    Input('input_button','n_clicks'),
+    State('input_url','value')
+)
+def load_new_material(n_clicks,url):
+    if 'input_button' == ctx.triggered_id:
+        db.add_opticalindex(url)
+    return [{'id':d[0],'name':d[1]} for d in db.get_material_list()],''
 
 @app.callback(
     Output('nk_graph','figure'),
@@ -27,16 +43,17 @@ app.layout = dbc.Container([
     State('material_list_table','data')
 )
 def show_selected_nk(selected_rows,tbl):
-    if not selected_rows:
-        selected_rows = [0]
-    id = tbl[selected_rows[0]]['id']
-    nks = db.get_opticalindex(id)
-    ns = [nk.real for nk in nks[1]]
-    ks = [-nk.imag for nk in nks[1]]
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=nks[0],y=ns,mode='lines',name='n'))
-    fig.add_trace(go.Scatter(x=nks[0],y=ks,mode='lines',name='k'))
-    return fig,selected_rows
+    if tbl:
+        id = tbl[selected_rows[0]]['id']
+        nks = db.get_opticalindex(id)
+        ns = [nk.real for nk in nks[1]]
+        ks = [-nk.imag for nk in nks[1]]
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=nks[0],y=ns,mode='lines',name='n'))
+        fig.add_trace(go.Scatter(x=nks[0],y=ks,mode='lines',name='k'))
+        return fig,selected_rows
+    else:
+        return go.Figure(),[]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
