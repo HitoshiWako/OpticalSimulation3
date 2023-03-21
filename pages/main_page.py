4095e69f6f442678aebfdc7aed455fb680450155
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2023-03-21 18:32:56 +0900

    show substrate reflectance

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,11 @@
 import dash
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
+import numpy as np
 from dash import html,dcc,callback,Input,Output,State,dash_table
 
 import opticalsimulation.database as db
-
-#substrate_list =[item[1]  for item in db.get_material_list()]
+import opticalsimulation.opticalsimulation as op
 
 dash.register_page(__name__,path='/')
 
@@ -16,15 +17,15 @@ layout = dbc.Container([
     [
         dbc.Col([
             dbc.Tabs([
-                dbc.Tab(dcc.Graph(id='graph_reflect'),label='Reflectance'),
-                dbc.Tab(dcc.Graph(id='graph_trans'),label='Transmittance'),
-                dbc.Tab(dcc.Graph(id='graph_reflect_with_back'),label='Reflectance(with back reflection)')
+                dbc.Tab(dcc.Graph(id='graph-reflect'),label='Reflectance'),
+                dbc.Tab(dcc.Graph(id='graph-trans'),label='Transmittance'),
+                dbc.Tab(dcc.Graph(id='graph-reflect-with-back'),label='Reflectance(with back reflection)')
             ])
         ]),
         dbc.Col([
             html.Div([
-                dbc.Label(id='label_angle'),
-                dcc.Slider(id='slider_angle',min=0,max=89.9,step=0.1,value=0,marks={0:'0',5:'5',12:'12',45:'45'}),
+                dbc.Label(id='label-angle'),
+                dcc.Slider(id='slider-angle',min=0,max=89.9,step=0.1,value=0,marks={0:'0',5:'5',12:'12',45:'45'}),
             ]),
             html.Br(),
             html.Div([
@@ -55,9 +56,9 @@ layout = dbc.Container([
                 dcc.Dropdown(id='substrate',options=[{'label':mat[1],'value':mat[0]} for mat in db.get_material_list()]),
                 dbc.InputGroup([
                     dbc.InputGroupText('Thcikness'),
-                    dbc.Input(type='number',min=0),
+                    dbc.Input(id='substrate-thickness',type='number',min=0),
                     dbc.InputGroupText('mm')
-                ],style={'padding-top':10}),
+                ], style={'padding-top':10}),
             ])
         ],width=3)
     ]
@@ -65,11 +66,46 @@ layout = dbc.Container([
 ])
 
 @callback(
-    Output('label_angle','children'),
-    Input('slider_angle','value')
+    Output('label-angle','children'),
+    Output('graph-reflect','figure'),
+    Output('graph-trans','figure'),    
+    Output('graph-reflect-with-back','figure'),
+    Input('slider-angle','value'),
+    Input('substrate','value'),
+    Input('substrate-thickness','value')
 )
-def update_angle(angle):
-    return 'Incident Angle: {:.1f}'.format(angle)
+def update_angle(angle,substrate,thickness):
+    fig_ref1 = go.Figure()
+    fig_ref2 = go.Figure()
+    fig_trans = go.Figure()
+
+    if substrate is not None:
+        nks = db.get_opticalindex(substrate)
+        min_wl = min(nks[0])
+        max_wl = max(nks[0])
+        step=10
+        wl = np.arange(min_wl,max_wl,step)
+        n0 = 1
+        n2 = 1
+        n1 = np.array(db.fitted_opticalindex(substrate,min_wl,max_wl,step))
+        if thickness is not None:            
+            ref1,trans,ref2,_ = op.calc_spectra(n0,n1,n2,angle,[],[],[],[],thickness,wl)
+            fig_ref1.add_trace(go.Scatter(x=wl,y=ref1[0], mode='lines'))
+            fig_ref1.add_trace(go.Scatter(x=wl,y=ref1[1], mode='lines'))
+            fig_trans.add_trace(go.Scatter(x=wl,y=trans[0], mode='lines'))
+            fig_trans.add_trace(go.Scatter(x=wl,y=trans[1], mode='lines'))
+            fig_ref2.add_trace(go.Scatter(x=wl,y=ref1[0], mode='lines'))
+            fig_ref2.add_trace(go.Scatter(x=wl,y=ref1[1], mode='lines'))
+        else:
+            thickness=1
+            ref1,trans,_,_ = op.calc_spectra(n0,n1,n2,angle,[],[],[],[],thickness,wl)
+            fig_ref1.add_trace(go.Scatter(x=wl,y=ref1[0], mode='lines'))
+            fig_ref1.add_trace(go.Scatter(x=wl,y=ref1[1], mode='lines'))
+            fig_trans.add_trace(go.Scatter(x=wl,y=trans[0], mode='lines'))
+            fig_trans.add_trace(go.Scatter(x=wl,y=trans[1], mode='lines'))
+            fig_ref2.add_trace(go.Scatter(x=wl,y=ref1[0], mode='lines'))
+            fig_ref2.add_trace(go.Scatter(x=wl,y=ref1[1], mode='lines'))
+    return 'Incident Angle: {:.1f}'.format(angle),fig_ref1,fig_trans,fig_ref2
 
 @callback(
     Output('table_layer','data'),
